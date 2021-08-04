1d018d9b8adfc12b8649c6056d62cee6ba442245
pochi13 <pochi.com777@gmail.com>
2021-08-04 15:09:56 +0900

    Revert "Revert "Merge pull request #22 from pochi13/user管理機能""
    This reverts commit 5f5384bf4d080a7d4b8ffa015964425c4292c02e.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,25 @@
 class User < ApplicationRecord
-  validates :nickname, presence: true
-  validates :email, presence: true
-  validates :encrypted_password, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birthday, presence: true
+  with_options presence: true do
+  validates :nickname
+  validates :birthday
+  
+   with_options format:{with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do # ここがユーザー本名全角の正規表現
+  validates :last_name
+  validates :first_name
+  
+   with_options format: {with: /\A[ァ-ヶー－]+\z/ } do# ここがユーザー本名全角の正規表現
+  validates :last_name_kana
+  validates :first_name_kana
+  end
+end
+end
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :comments,purchacese: :destroy
-         has_many :prototypes,items: :destroy
-
+       
 end
+
