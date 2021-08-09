ba648e389a3e166a31c1cb0d1d6bb562c474f522
pochi13 <pochi.com777@gmail.com>
2021-08-09 15:23:00 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :set_item, only: [:edit, :show, :update]
-  before_action :move_to_index, only: [:edit, :update]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.order("created_at DESC")
@@ -35,9 +35,10 @@ class ItemsController < ApplicationController
     end
   end
 
-
-
-
+    def destroy
+      @item.destroy
+      redirect_to root_path
+    end
 
     private
     def item_params
@@ -54,5 +55,3 @@ class ItemsController < ApplicationController
       end
     end
 end
-
-
