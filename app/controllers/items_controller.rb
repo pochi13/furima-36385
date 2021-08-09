4ba2fc033bc996d676bfc7a5537a02a65a94daca
pochi13 <pochi.com777@gmail.com>
2021-08-09 15:35:15 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :set_item, only: [:edit, :show, :update, :destroy]
+  before_action :set_item, expect: [:index, :new, :create]
   before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
