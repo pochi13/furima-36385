f21c8ccee89aa2445896116e38b79b15adcb63fc
pochi13 <pochi.com777@gmail.com>
2021-08-09 12:30:14 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,14 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :delete]
-
-
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:edit, :show, :update]　
+  before_action :move_to_index, except: [:edit, :update] 
 
   def index
     @items = Item.order("created_at DESC")
   end
 
   def show
-    @item = Item.find(params[:id])
-    
   end
 
   def new
@@ -27,16 +25,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
-    if @item.user == current_user
-      render :edit
-    else
-      redirect_to root_path
-    end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -53,7 +44,14 @@ class ItemsController < ApplicationController
       params.require(:item).permit(:image, :name, :explan, :detail_category_id, :detail_status_id, :delivery_charge_id, :delivery_area_id, :delivery_indication_id, :price).merge(user_id: current_user.id)
     end
 
+    def set_item
+    @item = Item.find(params[:id])
+    end
 
+    def move_to_index
+      if @item.user != current_user
+        redirect_to root_path
+    end
 
 
 end
