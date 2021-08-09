e625250e32b50f7b4044c03a32fdf4c8004efb50
pochi13 <pochi.com777@gmail.com>
2021-08-09 15:51:41 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, expect: [:index, :show, :destroy]
+  before_action :authenticate_user!, expect: [:index, :show]
   before_action :set_item, only: [:edit, :show, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy]
 
