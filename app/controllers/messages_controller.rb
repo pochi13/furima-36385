39fe3140b4607fae1b76596730321c7b8d8ca6d1
pochi13 <pochi.com777@gmail.com>
2021-09-07 19:05:55 +0900

    修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,6 +8,14 @@ class MessagesController < ApplicationController
     end
   end
 
+  def destroy
+    
+    @message = Message.find(params[:id])
+    @item = @message.item
+    @message.destroy
+    redirect_to item_path(@item)
+  end
+
   private
   def comment_params
     params.require(:message).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'items#index'
    resources :items do
-    resources :messages, only: [:create]
+    resources :messages, only: [:create,:destroy]
     resources :purchases,only:[:create,:index] 
     post 'order', on: :member
    end
